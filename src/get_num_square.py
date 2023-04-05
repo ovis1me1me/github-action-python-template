99d4677c4e6b97bdf186c31b8e28319f1b61528c
ovis1me1me <126409777+ovis1me1me@users.noreply.github.com>
2023-04-05 19:08:41 +0900

    Create get_num_square.py

---
+++ src/get_num_square.py
@@ -0,0 +1,13 @@
+#src/get_num_square.py
+import os
+
+num = os.environ.get("INPUT_NUM")
+if num:
+  try:
+    num = int(num)
+  except Exception:
+    exit('ERROR: the INPUT_NUM provided ('{}') is not an integer'.format(num))
+else:
+  num = 1
+
+print(f"::set-output name=num_sqared::{num**2}")
